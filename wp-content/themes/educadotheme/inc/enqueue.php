cfed5b4b2ae6cfe48a8771f407bd5a65fff981d0
Serhii <smolanovsergey@gmail.com>
2025-10-25 12:21:13 +0300

    new page

--- wp-content/themes/educadotheme/inc/enqueue.php
+++ wp-content/themes/educadotheme/inc/enqueue.php
@@ -2,7 +2,7 @@
 add_action('wp_enqueue_scripts', 'ed_scripts');
 function ed_scripts()
 {
-    $ver = '1.1.54';
+    $ver = '1.1.57';
     /* Style */
     wp_enqueue_style('ed-main', get_template_directory_uri() . '/assets/css/style.min.css', array(), $ver);
 
